46e1cd17310881829ea192fb2b629e4de52c0784
Carlos Zaburlin <zaburlin.carlos@gmail.com>
2024-10-07 17:29:16 -0300

    FEAT: update smtp server for godaddy

--- src/presentation/services/email.services.ts
+++ src/presentation/services/email.services.ts
@@ -5,21 +5,29 @@ export interface SendMailOptions {
   to: string | string[];
   subject: string;
   htmlBody: string;
-  attachments ?: Attachement[];
+  attachments?: Attachement[];
 }
 
 export interface Attachement {
   filename: string;
   path: string;
-  contentType:string
+  contentType: string;
 }
 
 export class EmailService {
   static async sendEmail(options: SendMailOptions): Promise<boolean> {
-    const { to, subject, htmlBody, attachments : attachments  = [] } = options;
+    const { to, subject, htmlBody, attachments: attachments = [] } = options;
 
     const transporter: Transporter = nodemailer.createTransport({
-      service: envs.MAILER_SERVICE,
+      // GoDaddy's SMTP server
+      host: "smtpout.secureserver.net", // GoDaddy's SMTP server
+      port: 465, // Use port 465 for SSL (or 587 for TLS if you change to TLS)
+      secure: true, // Use SSL for port 465 (set this to false for TLS with port 587)
+
+      // Gmail's SMTP server
+      //service: envs.MAILER_SERVICE,
+
+      // Common configuration for both GoDaddy and Gmail
       auth: {
         user: envs.MAILER_EMAIL,
         pass: envs.MAILER_SECRET_KEY,
@@ -32,7 +40,7 @@ export class EmailService {
         to: to,
         subject: subject,
         html: htmlBody,
-        attachments : attachments ,
+        attachments: attachments,
       });
 
       return true;
